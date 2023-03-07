d1af01524a02a515628e371f1c942aee60b72e7c
Gleb Dmitriev <woodrunsdeep@gmail.com>
2023-03-08 00:08:44 +0400

    feat(ui): add carousel

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,35 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/css';
+import './swiper-overrides.css';
+import Card from '../Card/Card';
+
+function Carousel({ deck, language }) {
+  return (
+    <Swiper
+      className="swiper"
+      spaceBetween={40}
+      centeredSlides
+      slideToClickedSlide
+      slidesPerView={1.5}
+      breakpoints={{
+        768: {
+          slidesPerView: 2.5,
+        },
+        1024: {
+          slidesPerView: 4.5,
+        },
+      }}
+    >
+      {deck.map((letter) => (
+        <SwiperSlide key={letter.id}>
+          <div className="swiper-slide-transform">
+            <Card letter={letter} language={language} />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default Carousel;
