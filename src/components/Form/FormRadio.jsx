c4280552f0aae6e036cd5f97125ab6214a83a1fe
Gleb Dmitriev <woodrunsdeep@gmail.com>
2023-03-11 14:20:34 +0400

    fix(ui): make form grid responsive

--- src/components/Form/FormRadio.jsx
+++ src/components/Form/FormRadio.jsx
@@ -4,10 +4,10 @@ import LanguageContext from '../../context/LanguageContext';
 function FormRadio({ radio }) {
   const language = useContext(LanguageContext);
   return (
-    <div>
+    <div className="form__radio">
       <input
         type="radio"
-        className="form__radio visually-hidden"
+        className="visually-hidden"
         name="option"
         id={radio.id}
         value={radio.name[language]}
