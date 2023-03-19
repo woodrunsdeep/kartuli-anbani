ebcbecd738f4e0a0008551fb6d213c9c4b5a9b05
Gleb Dmitriev <woodrunsdeep@gmail.com>
2023-03-19 21:43:05 +0400

    fix: remove wrapper

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -35,83 +35,81 @@ function Settings() {
         dispatch(saveSettings(data));
       }}
     >
-      <div className="settings__wrapper">
-        <h2 className="form__title">Game Settings</h2>
-        <fieldset>
-          <div className="form__options">
-            <legend>Language</legend>
-            <select name="language" id="language" defaultValue={language}>
-              {languages.map((l) => (
-                <option value={l} key={l}>{l}</option>
-              ))}
-            </select>
-          </div>
-        </fieldset>
-        <fieldset>
-          <div className="form__options">
-            <legend>Input Mode</legend>
-            {Object.values(inputModes).map((mode) => (
+      <h2 className="form__title">Game Settings</h2>
+      <fieldset>
+        <div className="form__options">
+          <legend>Language</legend>
+          <select name="language" id="language" defaultValue={language}>
+            {languages.map((l) => (
+              <option value={l} key={l}>{l}</option>
+            ))}
+          </select>
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form__options">
+          <legend>Input Mode</legend>
+          {Object.values(inputModes).map((mode) => (
+            <FormRadio
+              value={mode.type}
+              key={mode.type}
+              id={mode.type}
+              label={mode.icon}
+              name="inputMode"
+              defaultChecked={mode.type === inputMode}
+              onChange={(evt) => dispatch(setInputMode(evt.target.value))}
+            />
+          ))}
+        </div>
+      </fieldset>
+      <fieldset className="form__radio-options" disabled={inputMode === 'text' ? true : null}>
+        <legend>Options Number</legend>
+        <FormRange
+          min={inputModes.radio.min}
+          max={inputModes.radio.max}
+          name="optionsQty"
+          defaultValue={optionsQty}
+          id="optionsQty"
+        />
+      </fieldset>
+      <fieldset className="form__setting">
+        <div className="form__options form__options--column">
+          <legend>Deck Order</legend>
+          <div className="form__set">
+            {Object.values(deckOrderOptions).map((option) => (
               <FormRadio
-                value={mode.type}
-                key={mode.type}
-                id={mode.type}
-                label={mode.icon}
-                name="inputMode"
-                defaultChecked={mode.type === inputMode}
-                onChange={(evt) => dispatch(setInputMode(evt.target.value))}
+                value={option.type}
+                key={option.type}
+                id={option.type}
+                label={option.label}
+                name="deckOrder"
+                defaultChecked={option.type === deckOrder}
               />
             ))}
           </div>
-        </fieldset>
-        <fieldset className="form__radio-options" disabled={inputMode === 'text' ? true : null}>
-          <legend>Options Number</legend>
-          <FormRange
-            min={inputModes.radio.min}
-            max={inputModes.radio.max}
-            name="optionsQty"
-            defaultValue={optionsQty}
-            id="optionsQty"
-          />
-        </fieldset>
-        <fieldset className="form__setting">
-          <div className="form__options form__options--column">
-            <legend>Deck Order</legend>
-            <div className="form__set">
-              {Object.values(deckOrderOptions).map((option) => (
-                <FormRadio
-                  value={option.type}
-                  key={option.type}
-                  id={option.type}
-                  label={option.label}
-                  name="deckOrder"
-                  defaultChecked={option.type === deckOrder}
-                />
-              ))}
-            </div>
-          </div>
-        </fieldset>
-        <fieldset className="form__setting">
-          <div className="form__options form__options--column">
-            <legend>Theme</legend>
-            <div className="form__set">
-              {Object.values(themes).map((option) => (
-                <FormRadio
-                  value={option.type}
-                  key={option.type}
-                  id={option.type}
-                  label={option.icon}
-                  name="theme"
-                  defaultChecked={option.type === theme}
-                />
-              ))}
-            </div>
-          </div>
-        </fieldset>
-        <div className="form__actions">
-          <Button type="submit">Save</Button>
-          <Button type="reset">Reset</Button>
-          <Button onClick={() => dispatch(setVisibility(false))}>Cancel</Button>
         </div>
+      </fieldset>
+      <fieldset className="form__setting">
+        <div className="form__options form__options--column">
+          <legend>Theme</legend>
+          <div className="form__set">
+            {Object.values(themes).map((option) => (
+              <FormRadio
+                value={option.type}
+                key={option.type}
+                id={option.type}
+                label={option.icon}
+                name="theme"
+                defaultChecked={option.type === theme}
+              />
+            ))}
+          </div>
+        </div>
+      </fieldset>
+      <div className="form__actions">
+        <Button type="submit">Save</Button>
+        <Button type="reset">Reset</Button>
+        <Button onClick={() => dispatch(setVisibility(false))}>Cancel</Button>
       </div>
     </form>
   );
