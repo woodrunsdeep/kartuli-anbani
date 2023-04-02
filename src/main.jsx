7103161da49d88b558e331c6a66174034a596273
Gleb Dmitriev <woodrunsdeep@gmail.com>
2023-04-02 17:20:25 +0400

    feat(i18n): wire up react-i18next

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
+import i18n from './i18n';
 import './index.css';
 import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={null}>
+        <App />
+      </Suspense>
     </Provider>
   </React.StrictMode>,
 );
