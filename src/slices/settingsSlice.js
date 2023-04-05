cb12a0cebab0067551c6bd7d32b962ad995e8e62
Gleb Dmitriev <woodrunsdeep@gmail.com>
2023-04-05 15:00:36 +0400

    refactor: move static data from store to config

--- src/slices/settingsSlice.js
+++ src/slices/settingsSlice.js
@@ -2,67 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   language: 'en',
-  languages: ['en', 'ru'],
   optionsQty: 3,
-  inputModes: {
-    radio: {
-      type: 'radio',
-      label: 'Options',
-      icon: 'UiRadiosGrid',
-      min: 3,
-      max: 9,
-    },
-    text: {
-      type: 'text',
-      label: 'Text',
-      icon: 'InputCursorText',
-    },
-  },
-  isVisible: false,
   inputMode: 'radio',
   theme: 'system',
-  themes: {
-    light: {
-      type: 'light',
-      icon: 'Sun',
-    },
-    system: {
-      type: 'system',
-      icon: 'CircleHalf',
-    },
-    dark: {
-      type: 'dark',
-      icon: 'Moon',
-    },
-  },
   attemptsGiven: 3,
   deckOrder: 'alphabetical',
-  deckOrderOptions: {
-    alphabetical: {
-      type: 'alphabetical',
-      label: {
-        en: 'alphabetical',
-        ru: 'алфавитный',
-      },
-      icon: 'SortAlphaDown',
-    },
-    reverse: {
-      type: 'reverse',
-      label: {
-        en: 'reverse',
-        ru: 'обратный',
-      },
-      icon: 'SortAlphaDownAlt',
-    },
-    random: {
-      type: 'random',
-      label: {
-        en: 'случайный',
-        ru: 'случайный',
-      },
-      icon: 'Shuffle',
-    },
-  },
 };
 
 export const settingsSlice = createSlice({
